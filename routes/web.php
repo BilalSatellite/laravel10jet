7f16bd95c8c3a5bbfaa3e1a1d813d5da980a4a39
BilalSatellite <bilal.satellite@gmail.com>
2023-11-11 18:02:51 +0530

    hdd cheng new installation

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/panel/login', function () {
     return redirect('/login');
 })->name('filament.panel.auth.login');
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
